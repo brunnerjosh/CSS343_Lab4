72882adac96fcaed86e86f55af5b88cd9b88945a
Josh Brunner <joshuaebrunner@gmail.com>
2013-11-06 10:23:53 -0800

    Initial Commit

---
+++ Lab 4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab 4
+//
+//  Created by Josh Brunner on 11/6/13.
+//  Copyright (c) 2013 Josh Brunner. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
